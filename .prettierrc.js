b3cb0f8a33e2b6388349553319bee5a802d9b5b4
shiramashiro <zhengrenfu@outlook.com>
2021-10-20 23:12:24 +0800

    :art: 修改目录结构，重组代码

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,8 @@
 module.exports = {
-    // 使用单引号
     singleQuote: true,
-    // 行末尾是否有分号
     semi: false,
-    // 一行最多多少个字符
-    printWidth: 130,
-    // 末尾不需要逗号
+    printWidth: 200,
     trailingComma: 'none',
-    // 大括号内的首尾需要空格
     bracketSpacing: true,
     tabWidth: 4
 }
