b5fc3bcaad7c30f8d8f09a89ff34c9beb591d427
shiramashiro <zhengrenfu@outlook.com>
2022-07-08 01:26:51 +0800

    添加 Gitee 贡献图随机生成案例

--- src/03.案例/06.Gitee贡献图/js/index.js
+++ src/03.案例/06.Gitee贡献图/js/index.js
@@ -1,15 +1,64 @@
 function drawChartGrid() {
-  let $rightSide = $("#cb-chart .bottom .right-side");
-  let diamondSize = $rightSide.height() / 7;
-  let index = 0;
+  let pointIndex = 0;
+  let $el = $("#cb-chart .bottom .right-side");
+  $el.css({ "--point-size": `${ $el.height() / 7 }px` });
   for ( let horizontal = 0; horizontal < 53; horizontal++ ) {
     for ( let vertical = 0; vertical < 7; vertical++ ) {
-      $rightSide.css({ "--diamond-size": `${ diamondSize }px` });
-      $rightSide.append(`<div class="diamond" data-index="${ index }"></div>`);
-      index++;
+      $el.append(`<div class="point" data-index="${ pointIndex }"></div>`);
+      pointIndex++;
     }
   }
   drawRealDiamond();
+}
+
+function drawRealDiamond() {
+  let nowDate = new Date();
+  let oldDate = getLastYearToday(nowDate);
+  let nowWeekIndex = nowDate.getDay() - 1;
+  let oldWeekIndex = oldDate.getDay();
+  let points = getPointsData(oldDate);
+  if ( oldWeekIndex === 0 ) {
+    traverseDiamonds((i) => i > 5, nowWeekIndex, points);
+  } else if ( oldWeekIndex === 1 ) {
+    traverseDiamonds(() => true, nowWeekIndex, points);
+  } else if ( oldWeekIndex >= 2 ) {
+    traverseDiamonds((i) => i > oldWeekIndex - 2, nowWeekIndex, points);
+  }
+  drawMonths(oldDate);
+}
+
+function traverseDiamonds(valid, gap, points) {
+  $(`#cb-chart .bottom .right-side .point`).each((i, el) => {
+    if ( valid(i) ) {
+      if ( i === 365 + gap ) {
+        return false;
+      } else {
+        // 这个 if 判断只是临时的，避免报错，每天不管有没有贡献度都有数据对应
+        if ( points[i] ) {
+          $(el).attr("data-content", () => `${ points[i].num }个贡献：${ points[i].date }`);
+          setDiamondColor(el, points[i].num);
+        } else {
+          $(el).addClass("e");
+        }
+
+        $(el).on({
+          "mouseenter": () => {
+            showDiamondPopup(points[i], el);
+            $(el).addClass("point-hover")
+                 .next(".point-popup")
+                 .addClass("show-popup")
+                 .removeClass("close-popup");
+          },
+          "mouseleave": () => {
+            $(el).removeClass("point-hover")
+                 .next(".point-popup")
+                 .addClass("close-popup")
+                 .removeClass("show-popup");
+          }
+        });
+      }
+    }
+  });
 }
 
 function setDiamondColor(diamondDom, times) {
@@ -30,77 +79,22 @@ function showDiamondPopup(detailData, el) {
   // 这个 if 判断只是临时的，避免报错，每天不管有没有贡献度都有数据对应
   if ( detailData ) {
     $(el).after(`
-          <div class="diamond-popup close-popup" style="top: ${ el.offsetTop }px; left: ${ el.offsetLeft + el.clientWidth * 1.5 }px;">
-            <div>
-              ${ detailData.num }个贡献：${ detailData.date }
-            </div>
-          </div>
-        `);
+      <div class="point-popup close-popup" style="top: ${ el.offsetTop }px; left: ${ el.offsetLeft + el.clientWidth * 1.5 }px;">
+        <div>${ detailData.num }个贡献：${ detailData.date }</div>
+      </div>
+    `);
   }
 }
 
-function traverseDiamonds(validate, dayGap) {
-  $(`#cb-chart .bottom .right-side .diamond`).each((i, el) => {
-    if ( validate(i) ) {
-      if ( i === 365 + dayGap ) {
-        return false;
-      } else {
-        // 这个 if 判断只是临时的，避免报错，每天不管有没有贡献度都有数据对应
-        if ( diamonds[i] ) {
-          $(el).attr("data-content", () => `${ diamonds[i].num }个贡献：${ diamonds[i].date }`);
-          setDiamondColor(el, diamonds[i].num);
-        } else {
-          $(el).addClass("e");
-        }
-
-        $(el).on({
-          "mouseenter": () => {
-            showDiamondPopup(diamonds[i], el);
-            $(el).addClass("diamond-hover")
-                 .next(".diamond-popup")
-                 .addClass("show-popup")
-                 .removeClass("close-popup");
-          },
-          "mouseleave": () => {
-            $(el).removeClass("diamond-hover")
-                 .next(".diamond-popup")
-                 .addClass("close-popup")
-                 .removeClass("show-popup");
-          }
-        });
-      }
-    }
-  });
-}
-
 function drawMonths(oldDate) {
-  const months = [
-    "一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"
-  ];
   let oldDateMonthIndex = oldDate.getMonth();
   for ( let i = 0; i < 12; i++ ) {
     if ( oldDateMonthIndex === 12 ) {
       oldDateMonthIndex = 0;
     }
     $("#cb-chart .top-bar .months").append(`
-          <div class="month">${ months[oldDateMonthIndex] }</div>
-        `);
+      <div class="month">${ months[oldDateMonthIndex] }</div>
+    `);
     oldDateMonthIndex++;
   }
-}
-
-function drawRealDiamond() {
-  let nowDate = new Date();
-  let oldDate = getLastYearToday(nowDate);
-  let weekIndex = oldDate.getDay();
-  let nowWeekIndex = nowDate.getDay() - 1;
-  if ( weekIndex === 0 ) {
-    traverseDiamonds((i) => i > 5, nowWeekIndex);
-  } else if ( weekIndex === 1 ) {
-    traverseDiamonds(() => true, nowWeekIndex);
-  } else if ( weekIndex >= 2 ) {
-    traverseDiamonds((i) => i > weekIndex - 2, nowWeekIndex);
-  }
-
-  drawMonths(oldDate);
 }
--- src/03.案例/06.Gitee贡献图/js/utils.js
+++ src/03.案例/06.Gitee贡献图/js/utils.js
@@ -4,3 +4,26 @@ function getLastYearToday(date) {
   let day = date.getDate();
   return new Date(`${ year }-${ month }-${ day }`);
 }
+
+function getPointsData(date) {
+  let result = [];
+  let startYear = date.getFullYear();
+  let startMonth = date.getMonth();
+  let resultIndex = 0;
+  for ( let i = 0; i < 12; i++ ) {
+    let days = new Date(startYear, startMonth, 0).getDate();
+    for ( let d = 0; d < days; d++ ) {
+      result.push({
+        date: `${ startYear }-${ startMonth + 1 }-${ d + 1 }`,
+        num: Math.floor(Math.random() * 30)
+      });
+      resultIndex++;
+    }
+    startMonth++;
+    if ( startMonth >= 12 ) {
+      startMonth = 0;
+      startYear++;
+    }
+  }
+  return result;
+}
